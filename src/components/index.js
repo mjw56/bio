a34e60581a1fe86fe0a3580624aa7e451e1617fe
Mike Wilcox <mwilcox56@gmail.com>
2015-08-15 15:45:46 -0400

    rm body

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import Header from './Header';
-import Body from './Body';
 import Links from './Links';
 import Experience from './Experience';
 
-export { Header, Body, Links, Experience }
+export { Header, Links, Experience }
