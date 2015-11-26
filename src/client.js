7eca0cd19a1623c372065345efe7b709b20d62d6
Mike Wilcox <mwilcox56@gmail.com>
2015-11-26 09:20:37 -0500

    update router

--- src/client.js
+++ src/client.js
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Router from "react-router";
-import { history } from 'react-router/lib/BrowserHistory';
+import { createHistory } from 'history';
 import routes from "./routes";
 import { Provider } from 'react-redux';
 import configureStore from './lib/configureStore';
 
 const store = configureStore(window.__INITIAL_STATE__);
+const history = createHistory();
 
 ReactDOM.render(
   <Provider store={store}>
-    {() =>
       <Router children={routes} history={history} />
-    }
   </Provider>,
   document.getElementById('app')
 );
