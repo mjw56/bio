681f27dac5de94d389d288fc65fc5381cc937fb6
Mike Wilcox <mwilcox56@gmail.com>
2015-11-26 09:20:27 -0500

    update router

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import React from "react";
-import ReactDOMServer from "react-dom/server";
-import Router from "react-router";
-import Location from 'react-router/lib/Location';
+import { renderToString } from 'react-dom/server';
+import {RoutingContext, match} from "react-router";
+import { createLocation } from 'history'
 import routes from "./src/routes";
 import { Provider } from 'react-redux';
 import configureStore from './src/lib/configureStore';
@@ -12,53 +12,51 @@ const app = express();
 app.use(express.static(__dirname + "/"));
 
 app.use((req, res) => {
-  const location = new Location(req.path, req.query);
+  const location = createLocation(req.url);
 
-  Router.run(routes, location, (err, routeState) => {
+  const store = configureStore();
+
+  match({ routes, location: "/" }, (err, redirectLocation, renderProps) => {
+
     if(err) return console.error(err);
 
-    if(!routeState) return res.status(404).end('404');
-
-    const store = configureStore();
+    if(!renderProps) return res.status(404).end('404');
 
     function renderView() {
       const InitialView = (
         <Provider store={store}>
-          {() =>
-            <Router {...routeState} />
-          }
+            <RoutingContext {...renderProps} />
         </Provider>
       );
 
-      const componentHTML = ReactDOMServer.renderToStaticMarkup(InitialView);
+      const componentHTML = renderToString(InitialView);
 
       const initialState = store.getState();
 
       const HTML = `
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="utf-8">
-          <title>bio.</title>
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="utf-8">
+            <title>bio.</title>
 
-          <script>
-            window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
-          </script>
+            <script>
+              window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
+            </script>
 
-          <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-          <link href='/content/css/app.css' rel='stylesheet' type='text/css'>
-        </head>
-        <body class="theme-default">
-          <div id="app">${componentHTML}</div>
-          <script type="application/javascript" src="/static/bundle.js"></script>
-        </body>
-      </html>
-      `;
+            <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+            <link href='/content/css/app.css' rel='stylesheet' type='text/css'>
+          </head>
+          <body class="theme-default">
+            <div id="app">${componentHTML}</div>
+            <script type="application/javascript" src="/static/bundle.js"></script>
+          </body>
+        </html>`;
 
       return HTML;
     }
 
-    fetchComponentData(store.dispatch, routeState.components, routeState.params)
+    fetchComponentData(store.dispatch, renderProps.components)
       .then(renderView)
       .then(html => res.end(html))
       .catch(err => res.end(err.message));
